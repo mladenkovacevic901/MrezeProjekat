f981c141780ad924fec4972112cbb8158784be7f
mladenkovacevic901 <mladenkovacevic47@gmail.com>
2026-02-05 15:52:57 +0100

    Add project files.

---
+++ ProjekatMreze/Enumeratori.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatMreze
+{
+     public class Enumeratori
+    {
+
+       public enum TIP_UREDJAJA
+        {
+            VETROGENERATOR,
+            SOLARNI_PANEL,
+            BATERIJA,
+            POTROSAC
+
+        }
+
+      public   enum FIZICKA_VELICINA
+        {
+            P,
+            V,
+            C,
+            T   // pitati
+        }
+
+        public enum IO
+        {
+            ULAZ,
+            IZLAZ
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Projekat iz predmeta Primena računarskih mreža u infrastrukturnim sistemima. 
+Projekat simulira rad SCADA sistema. Omogućava prikaz regulacije nivoa napona i održavanja stabilnosti jednog ovakvog sistema sa više uređaja različitih tipova.
